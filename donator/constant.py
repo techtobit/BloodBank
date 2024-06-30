5407c5fc77010bfa50c1ad87d9aab16cc1fff700
techtobit <nurthern2016@gmail.com>
2024-06-30 23:11:55 +0600

    core - app created

---
+++ donator/constant.py
@@ -0,0 +1,10 @@
+BLOOD_GROUP = (
+	('A', 'A+ (Positive)'),
+	('A-', 'A- (Negitive)'),
+	('B+', 'B+ (Positive)'),
+	('B-', 'B- (Negitive)'),
+	('AB+', 'AB+ (Negitive)'),
+	('O+', 'O+ (Positive)'),
+	('O-', 'O- (Negitive)'),
+)
+
--- donator/models.py
+++ donator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from .constant import BLOOD_GROUP
 
-# Create your models here.
+class Donator(models.Model):
+	full_name =models.CharField(max_length=200)
+	age =models.IntegerField(blank=False)
+	blood_group =models.IntegerField(choices=BLOOD_GROUP)
+	# distric = 
+	# state = 
+	# uninion = 
+	total_donated =models.IntegerField(blank=False)
+	phone_number =models.IntegerField(blank=False)
