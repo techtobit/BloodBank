20cb51824e7f9e46934feaab5fcae8b4f23c01e8
techtobit <nurthern2016@gmail.com>
2024-06-30 23:26:39 +0600

    home url setup

--- BloodBank/urls.py
+++ BloodBank/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from core.views import Home
 
 urlpatterns = [
+    path('', Home, name='home'),
     path('admin/', admin.site.urls),
+    path('core/', include('core.urls'))
     # path('donator/', include('donator.urls')),
     # path('donator/', include('donator.urls')),
 ]
